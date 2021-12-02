82dca891c9f40b3cbff3d6ed3a90d6d8ce06a312
Steeves Saillant <steeves.saillant@alithya.com>
2021-12-02 15:55:25 -0500

    clean

--- src/main/java/prod/Calculator.java
+++
@@ -1,9 +0,0 @@
-package prod;
-
-import java.util.Arrays;
-
-public class Calculator {
-    public int Add(int[] numbers) {
-        return Arrays.stream(numbers).sum();
-    }
-}
--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -2,7 +2,6 @@ package hellocucumber;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 
